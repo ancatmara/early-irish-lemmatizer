503eabd8924e6c97c1da568c441300b99eeff348
Oksana Dereza <Oksana Dereza>
2016-06-15 19:30:15 +0300

    Changed some functions in test script

--- test_lemmatizer.py
+++ test_lemmatizer.py
@@ -16,27 +16,43 @@ def process_files(path):
     os.makedirs(path + "\\lemmatized", exist_ok=True)
     files = [name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))]
     for file in files:
-        with open(os.path.join(path, file), 'r', encoding='utf-8') as infile:
-            lem = Lemmatizer(infile.read())
-            totalUnlemmatized += lem.unlemmatized
-            totalRecall += lem.recall
-            print("Recall in %s: %s" % (file, lem.recall))
-        with open(path + '\\lemmatized\\lem_' + file, 'w', encoding='utf-8') as outfile:
-            outfile.write(lem.lemmaText)
+        print('Processing %s' % (file))
+        fileRecall = process_text(os.path.join(path, file), path + '\\lemmatized\\lem_' + file)
+        totalRecall += fileRecall
     totalRecall = totalRecall / len(files)
     print(totalRecall)
+    #totalProposed = predict_lemmas(totalUnlemmatized)
+    #return totalProposed
 
-    unlemmatizedCounts = Counter(totalUnlemmatized)
-    edits = Edits(unlemmatizedCounts, 1)
+def predict_lemmas(unlemmatized):
+    unlemmatizedCounts = Counter(unlemmatized)
+    edits = Edits(unlemmatizedCounts, 0)
     totalProposed = edits.proposed
-    with open('proposed_lemmas.txt', 'w', encoding= 'utf-8') as f1, open('unlemmatized.txt', 'w', encoding='utf-8') as f2:
+    with open('proposed_gold.txt', 'w', encoding= 'utf-8') as f1, open('unlemmatized_gold.txt', 'w', encoding='utf-8') as f2:
         for entry in sorted(totalProposed):
-            f1.write('%s    %s    %s\n' % (entry[0], entry[1], entry[2]))
+            f1.write('%s\t%s\t%s\n' % (entry[0], entry[1], entry[2]))
         for word in sorted(unlemmatizedCounts, key=unlemmatizedCounts.get):
-            f2.write('%s    %s\n' % (word, unlemmatizedCounts[word]))
+            f2.write('%s\t%s\n' % (word, unlemmatizedCounts[word]))
     return totalProposed
 
+def process_text(in_path, out_path):
+    with open(in_path, 'r', encoding='utf-8') as f, open(out_path, 'w', encoding='utf-8') as outfile:
+        totalUnlemmatized = []
+        totalRecall = 0
+        cnt = 0
+        for line in f:
+            lem = Lemmatizer(line)
+            totalUnlemmatized += lem.unlemmatized
+            totalRecall += lem.recall
+            cnt +=1
+            outfile.write(lem.lemmaText + '\n')
+    totalRecall = totalRecall / cnt
+    print("Recall: %s" % (totalRecall))
+    #predict_lemmas(totalUnlemmatized)
+    return totalRecall
 
 if __name__ == '__main__':
-    proposed = process_files(sys.argv[1])
+    # proposed = process_files('C:\\Users\\ahten_000\\Dropbox\\Библиотека\\Вышка\\OldIrish\\texts\\processed')
     # pp(sorted(proposed)[:25])
+    process_files('C:\\Users\\ahten_000\\Dropbox\\Библиотека\\Вышка\\OldIrish\\texts\\processed')
+
